4dc4eb02326735eb3fb213362a747a9acfe60da5
Jean <46204992+JeanVydes@users.noreply.github.com>
2021-07-23 18:07:08 -0500

    Update config.js

--- bot/config.js
+++ bot/config.js
@@ -1,11 +1,11 @@
 const webhooks = {
-        "shard": {
-        "id": "723758053354111209",
-        "token": "GZkHKy4WRBkU7ktmQby7R50Y6E-RmMWN2p-BmGqZ2BQBYHzXA4_fdM2VB8I0pcEDaR_U"
+    "shard": {
+        "id": "webhook id",
+        "token": "webhook token"
     },
-        "cluster": {
-        "id": "723757713758093354",
-        "token": "JLOG2NgZkwr9ppsMZT3NWUX7kRkwrKPLXV01D3eVGMusH6wHPzdTmzPy0d-WSSiSRjnr"
+    "cluster": {
+        "id": "webhook id",
+        "token": "webhook token"
     }
 }; 
 
