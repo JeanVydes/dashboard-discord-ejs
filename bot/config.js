8954bc5e2381241008f4f797305886ca7cfbef48
Jean <46204992+JeanVydes@users.noreply.github.com>
2021-07-23 18:06:19 -0500

    Add files via upload

---
+++ bot/config.js
@@ -0,0 +1,12 @@
+const webhooks = {
+        "shard": {
+        "id": "723758053354111209",
+        "token": "GZkHKy4WRBkU7ktmQby7R50Y6E-RmMWN2p-BmGqZ2BQBYHzXA4_fdM2VB8I0pcEDaR_U"
+    },
+        "cluster": {
+        "id": "723757713758093354",
+        "token": "JLOG2NgZkwr9ppsMZT3NWUX7kRkwrKPLXV01D3eVGMusH6wHPzdTmzPy0d-WSSiSRjnr"
+    }
+}; 
+
+module.exports = { webhooks }
