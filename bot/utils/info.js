cdb1a271a986ae2b914a81960623c8865c7c7696
Jean <46204992+JeanVydes@users.noreply.github.com>
2021-07-23 18:00:20 -0500

    Delete bot/utils directory

--- bot/utils/info.js
+++
@@ -1,5 +0,0 @@
-const coinName = "coins"
-
-module.exports = {
-    coinName
-}
--- bot/utils/require.js
+++
@@ -1,9 +0,0 @@
-const Eris = require("eris-additions")(require("eris"))
-const user = require("../mongo/models/user");
-const guild = require("../mongo/models/guild");
-const formatThis = require("humanize-duration");
-const c = require("../../config");
-const { inspect } = require("util");
-const { format } = require("./misc");
-
-module.exports = { Eris, formatThis, user, guild, c, inspect, format };
